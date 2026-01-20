907be0cfd678df7ce70c332f14e13c01c040ddd4
LG PC <LG PC@DESKTOP-7VMKV5D>
2026-01-20 18:06:32 +0900

    commit

---
+++ src/main/java/exgit/ExgitApplication.java
@@ -0,0 +1,13 @@
+package exgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exgit/ExgitApplicationTests.java
@@ -0,0 +1,13 @@
+package exgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
